e8a36b33b4a64f7783628fd4f0e414249544ae5b
murvancev39 <murvancev39@gmail.com>
2026-04-27 11:54:45 +0300

    some changes in hash tables

--- lab4/include/hash_table_open.h
+++ lab4/include/hash_table_open.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef enum
 {
@@ -13,27 +14,35 @@ typedef enum
 #define EMPTY ((void *) 0)
 #define DELETED ((void *) 1)
 
+
+typedef int (*cmp_t) (void *, void *);
+typedef unsigned (*hash_f_t) (unsigned, void *);
+
 struct Open_Hash_Table
 {
     void **arr;
     size_t size;
     size_t count;
     float load_factor;
-    unsigned (*hash_f1) (struct Open_Hash_Table *, void *);
-    unsigned (*hash_f2) (struct Open_Hash_Table *, void *);
-    unsigned (*idx_f)   (struct Open_Hash_Table *, unsigned cur_num, unsigned cur_step);
+    cmp_t cmp;
+    hash_f_t hash_f1;
+    unsigned koef_for_f1;
+    hash_f_t hash_f2;
+    unsigned koef_for_f2;
+    unsigned (*idx_f) (struct Open_Hash_Table *, void *, unsigned);
+    unsigned max_iterations;
 };
 
 typedef struct Open_Hash_Table open_hash_table_t;
+typedef unsigned (*idx_f_t)   (open_hash_table_t *, void *, unsigned);
 
-typedef unsigned (*hash_f_t) (open_hash_table_t *, void *);
-typedef unsigned (*idx_f_t)   (open_hash_table_t *, unsigned cur_num, unsigned cur_step);
+
 
 open_hash_table_t *open_hash_table_ctr (size_t size, float load_factor);
 void open_hash_table_dtr (open_hash_table_t *table);
 unsigned hash (void *key);
 unsigned hash_2 (void *key);
-void open_hash_table_rehash (open_hash_table_t *table);
+void open_hash_table_rehash (open_hash_table_t *table, int add_f (open_hash_table_t *, void *));
 
 
 #endif
